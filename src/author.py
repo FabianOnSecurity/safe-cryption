4ed1940f179b48d2935badb4f35b901b01a097a0
Fabian Gieshoff <fabian.gieshoff@rub.de>
2022-05-17 21:24:52 +0200

    Create author.py

---
+++ src/author.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+#License Information
+
+__author__ = "Maxim Steib, Fabian Gieshoff"
+__copyright__ = "Copyright 2022, Safe-Cryption"
+__license__ = "GPL"
+__version__ = "0.0.1"
+__status__ = "Production"
