cdedb770d0af6bf5a5fd24ac48c995214c7090cc
Sasorie42 <96379081+Sasorie42@users.noreply.github.com>
2022-05-21 14:40:34 +0200

    +function prepare

--- src/sound_gen.py
+++ src/sound_gen.py
@@ -3,42 +3,40 @@ from random import seed,randrange
 import wave
 #from pydub import AudioSegment
 
-src = "Versuch 3.wav"
-#srclen = len(src)
-#if src[srclen-3:] == "mp3":
-    #sound = AudioSegment.from_mp3(src)
-    #src = src[:srclen-3]+"wav"
-    #sound.export(src, format="wav")
-#print(src)
+def prepare(src):
+    #srclen = len(src)
+    #if src[srclen-3:] == "mp3":
+        #sound = AudioSegment.from_mp3(src)
+        #src = src[:srclen-3]+"wav"
+        #sound.export(src, format="wav")
+    craze = wave.open(src,"r")
+    crazebytes = craze.readframes(-1)
+    crazebits = np.frombuffer(crazebytes, dtype='int16')
+    crazelist = crazebits.tolist()
+    for i in range(0,18,1):
+        while i in crazelist:
+            crazelist.remove(i)
+        while -i in crazelist:
+            crazelist.remove(-i)
+    return crazelist
 
-Welle = wave.open(src,"r")
-Wellenbytes = Welle.readframes(-1)
-Wellenbits = np.frombuffer(Wellenbytes, dtype='int16')
-Wellenliste = Wellenbits.tolist()
-for i in range(0,18,1):
-    while i in Wellenliste:
-        Wellenliste.remove(i)
-    while -i in Wellenliste:
-        Wellenliste.remove(-i)
-
-#print(Wellenliste)
-def Soundgen(Wellenliste,keylen):
+def Soundgen(crazelist,keylen):
     x = 0
     for j in range(0,keylen//8,1):
         for l in range(0,16,1):
-            y = Wellenliste[x]
+            y = crazelist[x]
             if y < 0 :
                 y = -y
             if l == 0:
-                Keyrunde = (y)
+                Keyround = (y)
             else:
-                Keyrunde = Keyrunde ^ y
+                Keyround = Keyround ^ y
             y = y % 128
-            if x+y>= len(Wellenliste):
+            if x+y>= len(crazelist):
                 x = j+1
             else:
                 x = x + y
-        value = str(hex(Keyrunde))[2:4]
+        value = str(hex(Keyround))[2:4]
         if len(value) < 2:
             seed()
             padding = randrange(1, 9)
@@ -51,4 +49,7 @@ def Soundgen(Wellenliste,keylen):
     print(len(final_key))
     return final_key
 
-Soundgen(Wellenliste,256)
+
+src = "Versuch 3.wav"
+crazelist = prepare(src)
+Soundgen(crazelist,256)
